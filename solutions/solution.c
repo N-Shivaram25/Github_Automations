740b3c4ee93c726b737362b909321f8e1ead54c4
Shiva Ram <naradashishivaram25@gmail.com>
2025-04-03 14:43:26 +0530

    Initial commit

---
+++ solutions/solution.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void sortedSquares(int* nums, int size, int* result) {
+    int left = 0, right = size - 1, pos = size - 1;
+
+    while (left <= right) {
+        if (abs(nums[left]) > abs(nums[right])) {
+            result[pos] = nums[left] * nums[left];
+            left++;
+        } else {
+            result[pos] = nums[right] * nums[right];
+            right--;
+        }
+        pos--;
+    }
+}
