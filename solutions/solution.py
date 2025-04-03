e6e5bb0f31608104a0cf6e61d2e4cecc7e97c347
Shiva Ram <naradashishivaram25@gmail.com>
2025-04-03 15:49:22 +0530

    ymlls

--- solutions/solution.py
+++ solutions/solution.py
@@ -18,7 +18,7 @@ class Solution:
                 result[index] = right_square
                 right -= 1
 
-            index -= 1  # Move to the next position
+            index -= 1  # Move to the next position543454
 
         return result#asfsw
 
