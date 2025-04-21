dd02357362aaca775f0fd01f1fd43a3527317a9d
Shiva Ram <naradashishivaram25@gmail.com>
2025-04-21 15:33:35 +0530

    c 4

--- solutions/solution.c
+++ solutions/solution.c
@@ -29,7 +29,7 @@ struct Node* buildLinkedList(int arr[], int size) {
             tail = newNode;
         } else {
             tail->next = newNode;
-            tail = newNode;
+            tail = newNode;//kghkhkh
         }
     }
     return head;
