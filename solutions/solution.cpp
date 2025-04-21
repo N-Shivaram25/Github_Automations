ecd62e0f26ec10733c75403fece9c2de1227e377
Shiva Ram <naradashishivaram25@gmail.com>
2025-04-21 21:32:05 +0530

    converted to base codes

--- solutions/solution.cpp
+++ solutions/solution.cpp
@@ -3,18 +3,7 @@ struct Node {
     Node* next;
 };
 
-class Solution {
-    public:
-      // Function to search for a key in the linked list.
-      bool searchKey(int n, Node* head, int key) {
-          Node* current = head;
-          while (current != nullptr) {
-              if (current->data == key) {
-                  return true;//fdfgd  //dsfsdgsd
-              }
-              current = current->next;//sfsfgg
-          }
-          return false;
-      }  
-  };
+/*===============================================================================================================================
+Write code here
+*/
   
