104cbab2284c633a5757f7a2dcb16626160f020b
Shiva Ram <naradashishivaram25@gmail.com>
2025-04-21 15:36:37 +0530

    py 1

--- solutions/solution.py
+++ solutions/solution.py
@@ -11,3 +11,13 @@ class Solution:
                 return True
             current = current.next
         return False
+
+def build_linked_list(arr):
+    if not arr:
+        return None
+    head = Node(arr[0])
+    current = head
+    for val in arr[1:]:
+        current.next = Node(val)
+        current = current.next
+    return head
