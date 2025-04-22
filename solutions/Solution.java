bbb4f17d7da42e754a19f4d24f709888da75a774
Shiva Ram <naradashishivaram25@gmail.com>
2025-04-22 10:34:15 +0530

    java

--- solutions/Solution.java
+++ solutions/Solution.java
@@ -7,8 +7,24 @@ public class Solution {
             next = null;
         }
     }
-    /*=========================================================================================================================
-    Write the code here
-    */
-   /*/
+
+    public static Node buildLinkedList(int[] arr, int n) {
+        if (n == 0) return null;
+        Node head = new Node(arr[0]);
+        Node curr = head;
+        for (int i = 1; i < n; i++) {
+            curr.next = new Node(arr[i]);
+            curr = curr.next;
+        }
+        return head;
+    }
+
+    public static boolean searchKey(int n, Node head, int key) {
+        Node current = head;
+        while (current != null) {
+            if (current.data == key) return true;
+            current = current.next;
+        }
+        return false; ///sdfsdfsd     //sdgsgsdgggdsgd
+    }
 }
