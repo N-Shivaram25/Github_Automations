48f86ef00374aa60633dbdc7bd9911aa49f38b55
Shiva Ram <naradashishivaram25@gmail.com>
2025-04-22 11:20:14 +0530

    c

--- solutions/solution.c
+++ solutions/solution.c
@@ -1,18 +1,30 @@
-#include <stdlib.h>
-#include <stdbool.h>
+public class Solution {
+    static class Node {
+        int data;
+        Node next;
+        Node(int d) {
+            data = d;
+            next = null;
+        }
+    }
 
+    public static Node buildLinkedList(int[] arr, int n) {
+        if (n == 0) return null;
+        Node head = new Node(arr[0]);
+        Node curr = head;
+        for (int i = 1; i < n; i++) {
+            curr.next = new Node(arr[i]);00000
+            curr = curr.next;
+        }
+        return head;
+    }
 
-struct Node {
-    int data;
-    struct Node* next;//v
-};
-
-//dsgdsgsdsssssffsss
-bool searchKey(int n, struct Node* head, int key) {
-    // write your code heredddsss
-}
-
-
-struct Node* buildLinkedList(int arr[], int size) {
-    // write your code here
+    public static boolean searchKey(int n, Node head, int key) {
+        Node current = head;
+        while (current != null) {
+            if (current.data == key) return true;
+            current = current.next;
+        }
+        return false; ///sdfsdfsd     //sdgsgsdgggdsgd
+    }
 }
