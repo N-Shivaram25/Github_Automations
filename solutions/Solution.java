2cb52510741471c9018c259c9d12aff22c474092
Shiva Ram <naradashishivaram25@gmail.com>
2025-04-22 12:59:38 +0530

    yml

--- solutions/Solution.java
+++ solutions/Solution.java
@@ -13,7 +13,7 @@ public class Solution {
         Node head = new Node(arr[0]);
         Node curr = head;
         for (int i = 1; i < n; i++) {
-            curr.next = new Node(arr[i]);00000
+            curr.next = new Node(arr[i]);
             curr = curr.next;
         }
         return head;
