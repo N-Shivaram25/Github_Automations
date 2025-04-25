8e0c7773aa969c0c28a7e6e27cfdbde2003c0870
Shiva Ram <naradashishivaram25@gmail.com>
2025-04-25 13:49:34 +0530

    c

--- solutions/solution.c
+++ solutions/solution.c
@@ -1,18 +1,36 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-
 struct Node {
     int data;
-    struct Node* next;//v
+    struct Node* next;
 };
 
-//dsgdsgsdsssssffsss
 bool searchKey(int n, struct Node* head, int key) {
-    // write your code heredddsss
+    struct Node* current = head;
+    while (current != NULL) {
+        if (current->data == key) {
+            return true;
+        }
+        current = current->next;
+    }
+    return false;
 }
 
-
 struct Node* buildLinkedList(int arr[], int size) {
-    // write your code heres
+    struct Node* head = NULL;
+    struct Node* tail = NULL;
+    for (int i = 0; i < size; i++) {
+        struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode->data = arr[i];
+        newNode->next = NULL;
+        if (head == NULL) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail->next = newNode;
+            tail = newNode;//kghkhkh/sdsdfsd /sdfsdfa   //sdvdfd //asdsd  //sdsdfgdsg   //hdfhdfhd
+        }
+    }
+    return head;
 }
