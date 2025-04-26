7ed0e34e764554d4268c4d8fc2f225bd5b632169
Shiva Ram <naradashishivaram25@gmail.com>
2025-04-26 15:39:36 +0530

    new

--- solutions/Solution.java
+++ solutions/Solution.java
@@ -8,7 +8,7 @@ public class Solution {
         }
     }
 //ssdasacsdcsssssssssssssssssss
-    public static Node buildLinkedList(int[] arr, int n) {
+    public static Node buildLinkedsList(int[] arr, int n) {
         if (n == 0) return null;
         Node head = new Node(arr[0]);
         Node curr = head;
