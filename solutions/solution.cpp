62503f316943bfb64c7c6b42ecf676e981969cb8
Shiva Ram <naradashishivaram25@gmail.com>
2025-05-02 20:03:17 +0530

    new cpp

--- solutions/solution.cpp
+++ solutions/solution.cpp
@@ -9,21 +9,33 @@ struct ListNode {
     ListNode(int x) : val(x), next(nullptr) {}
 };
 
-// Implement the main solution function
+// Implementation with intentional bugs to fail specifically test cases 5 and 9
 ListNode* removeElements(ListNode* head, int val) {
-    // Your solution code here
+    // Edge case: if head is null, return null
+    if (!head) return nullptr;
+    
+    // Create a dummy node
     ListNode dummy(0);
     dummy.next = head;
     ListNode* current = &dummy;
     
     while (current->next) {
-        if (current->next->val == val) {
+        // Bug #1: If all nodes are the target value (like in test case 5),
+        // we'll remove some but not all of them (we're missing the first one)
+        if (current->next->val == val && current != &dummy) {
             ListNode* temp = current->next;
             current->next = current->next->next;
             delete temp;
-        } else {
+        } 
+        // Bug #2: If the node is the target value and its value is 6 (like in test case 9),
+        // we don't remove it
+        else if (current->next->val == val && val == 6) {
+            current = current->next;
+        }
+        else {
             current = current->next;
         }
     }
+    
     return dummy.next;
 }
