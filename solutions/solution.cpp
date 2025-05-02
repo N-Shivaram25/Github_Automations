eec3332469f565702de66b328a5b8fde6823890c
Shiva Ram <naradashishivaram25@gmail.com>
2025-05-02 15:18:47 +0530

    solution full.cpp Add

--- solutions/solution.cpp
+++ solutions/solution.cpp
@@ -1,6 +1,56 @@
+#include <vector>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
 class Solution {
-    public:
-        ListNode* removeElements(ListNode* head, int val) {
-            
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode dummy(0);
+        dummy.next = head;
+        ListNode* current = &dummy;
+
+        while (current->next) {
+            if (current->next->val == val) {
+                ListNode* temp = current->next;
+                current->next = current->next->next;
+                delete temp;
+            } else {
+                current = current->next;
+            }
         }
-    };
+        return dummy.next;
+    }
+};
+
+// Utility Functions (needed for test.cpp)
+ListNode* createList(const vector<int>& vals) {
+    ListNode dummy(0);
+    ListNode* tail = &dummy;
+    for (int v : vals) {
+        tail->next = new ListNode(v);
+        tail = tail->next;
+    }
+    return dummy.next;
+}
+
+vector<int> toVector(ListNode* head) {
+    vector<int> result;
+    while (head) {
+        result.push_back(head->val);
+        head = head->next;
+    }
+    return result;
+}
+
+void deleteList(ListNode* head) {
+    while (head) {
+        ListNode* temp = head;
+        head = head->next;
+        delete temp;
+    }
+}
