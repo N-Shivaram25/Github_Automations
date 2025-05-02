a5e29c3d9e2270537dc813bff68d3213e22753b7
Shiva Ram <naradashishivaram25@gmail.com>
2025-05-02 20:34:53 +0530

    new cpp

--- solutions/solution.c
+++ solutions/solution.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 // Define the ListNode structure
 struct ListNode {
@@ -11,8 +12,43 @@ struct ListNode {
  * 
  * @param head pointer to the head of the list
  * @param val value to be removed
- * @return pointer to the new head of the modified lists
+ * @return pointer to the new head of the modified list
  */
 struct ListNode* removeElements(struct ListNode* head, int val) {
-   
+    // BUG #1: Directly return NULL for empty list instead of properly handling it
+    if (head == NULL) {
+        return NULL;
+    }
+    
+    // BUG #2: Only remove the first occurrence of the value
+    if (head->val == val) {
+        struct ListNode* newHead = head->next;
+        free(head);
+        return newHead;
+    }
+    
+    // Create a pointer to traverse the list
+    struct ListNode* current = head;
+    
+    // Traverse the list (but only remove the first occurrence we find)
+    while (current != NULL && current->next != NULL) {
+        if (current->next->val == val) {
+            // Save the node to be deleted
+            struct ListNode* temp = current->next;
+            
+            // Bypass the node to be removed
+            current->next = current->next->next;
+            
+            // Free the memory of the removed node
+            free(temp);
+            
+            // Break after removing one occurrence (BUG)
+            break;
+        } else {
+            // Move to the next node
+            current = current->next;
+        }
+    }
+    
+    return head;
 }
