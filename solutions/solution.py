fa7583ee0405a5608f1e9cb7398081eca09d1d6a
Shiva Ram <naradashishivaram25@gmail.com>
2025-05-03 16:10:14 +0530

    py

--- solutions/solution.py
+++ solutions/solution.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-# Definition for singly-linked lists.
+# Definition for singly-linked listss.
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
