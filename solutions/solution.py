5cc7c478b8c78c21466094ecd42f2652bc4a98c5
Shiva Ram <naradashishivaram25@gmail.com>
2025-05-04 15:34:55 +0530

    py

--- solutions/solution.py
+++ solutions/solution.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-# Definition for singly-linked listss.s
+# Definition for singly-linked listss.ss
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
